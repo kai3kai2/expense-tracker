95eae9500f35b609ec3f5642dd1d489fb6c3fbfa
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2023-02-12 21:06:55 +0800

    feat: add CRUD function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const Record = require("./models/record");
+const bodyParser = require("body-parser");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 app.engine("hbs", exphbs({ extname: ".hbs", defaultLayout: "main" }));
 app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mongoose.connection;
 
@@ -30,6 +32,46 @@ app.get("/", (req, res) => {
   Record.find()
     .lean()
     .then((records) => res.render("index", { records }))
+    .catch((err) => console.log(err));
+});
+app.get("/records/new", (req, res) => {
+  return res.render("new");
+});
+
+app.post("/records", (req, res) => {
+  const { name, amount, Date } = req.body;
+  return Record.create({ name, amount, Date })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+app.get("/records/:id/edit", (req, res) => {
+  const id = req.params.id;
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render("edit", { record }));
+});
+
+app.post("/records/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const { name, amount, Date } = req.body;
+
+  return Record.findById(id)
+    .then((record) => {
+      record.name = name;
+      record.amount = amount;
+      record.Date = Date;
+      return record.save();
+    })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
+});
+
+app.post("/records/:id/delete", (req, res) => {
+  const id = req.params.id;
+  return Record.findById(id)
+    .then((record) => record.remove())
+    .then(() => res.redirect("/"))
     .catch((err) => console.log(err));
 });
 
