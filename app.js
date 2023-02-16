9c4b4ade617e66ad690e6ab1729cf716dbbd1a79
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2023-02-16 22:22:50 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const Record = require("./models/record");
 const bodyParser = require("body-parser");
@@ -11,6 +12,14 @@ const PORT = 3000;
 
 app.engine("hbs", exphbs({ extname: ".hbs", defaultLayout: "main" }));
 app.set("view engine", "hbs");
+app.use(
+  session({
+    secret: process.env.SESSION,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(routes);
