bc0e1478261e8bb2ef543da7eafc9304039aab0c
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2023-02-16 22:42:43 +0800

    feat: add logout function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -15,6 +15,11 @@ router.post(
   })
 );
 
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/users/login");
+});
+
 router.get("/register", (req, res) => {
   res.render("register");
 });
