015b669f36168806e49f6571ed91fd25a7a3ce62
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2023-02-18 13:24:20 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -12,13 +12,9 @@ router.post(
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/users/login",
+    failureMessage: true,
   })
 );
-
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/users/login");
-});
 
 router.get("/register", (req, res) => {
   res.render("register");
@@ -27,28 +23,52 @@ router.get("/register", (req, res) => {
 router.post("/register", (req, res) => {
   // 取得輸入資料
   const { name, email, password, confirmPassword } = req.body;
+  const errors = [];
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: "所有欄位都是必填。" });
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: "密碼與密碼確認不相符!" });
+  }
+  if (errors.length) {
+    return res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    });
+  }
   // 檢查註冊狀態
   User.findOne({ email })
     .then((user) => {
       if (user) {
-        console.log("User already exists.");
-        res.render("register", {
+        errors.push({ message: "信箱已經註冊過了!" });
+        console.log(email);
+        return res.render("register", {
+          errors,
           name,
           email,
           password,
           confirmPassword,
         });
-      } else {
-        return User.create({
-          name,
-          email,
-          password,
-        })
-          .then(() => res.redirect("/"))
-          .catch((err) => console.log(err));
       }
+      return User.create({
+        name,
+        email,
+        password,
+      })
+        .then(() => res.redirect("/"))
+        .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "你已經成功登出。");
+  res.redirect("/users/login");
 });
 
 module.exports = router;
