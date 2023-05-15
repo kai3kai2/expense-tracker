965885f671b6f21b0bbee464538ba5cb8a2380a0
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-05-15 19:05:28 +0800

    feat : add google login

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -18,4 +18,17 @@ router.get(
   })
 );
 
+router.get('/google',
+  passport.authenticate('google', {
+    scope: ["email", "profile"]
+  })
+)
+
+router.get('/google/callback', 
+  passport.authenticate('google', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  })
+)
+
 module.exports = router;
